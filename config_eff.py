c754f628bcc111909b5e6045254ed524be921b7b
Jacopo Dalmasson <jdalmass@rice14.stanford.edu>
2017-11-27 10:26:28 -0800

    paper graphics

--- config_eff.py
+++ config_eff.py
@@ -8,7 +8,7 @@ def tick_function(X):
 	V = np.asarray(map(str,100000/X.astype(int)))
 	V[11::2] = ''
 	return V
-
+g4_plot.set_style()
 ax1 = plt.subplot()
 ax2 = ax1.twiny()
 ax1.set_xscale('log')
@@ -25,7 +25,7 @@ for sl in seed_loc:
 		if read_conf['EPD_ratio'] == 0.5:
 			continue
 		try:
-			with h5py.File(cfg+sl+'e-_sim.h5','r') as f:
+			with h5py.File('%sraw_data/%se-_sim.h5'%(cfg,sl),'r') as f:
 				e_index = np.diff(f['idx_tr'][:])
 				tot_index = np.diff(f['idx_depo'][:])
 				ax1.errorbar(px_per_lens,np.mean(e_index/np.mean(tot_index)),np.std(e_index/np.mean(tot_index))/np.sqrt(len(e_index)),color='black')#fmt='o'
@@ -52,8 +52,8 @@ ax1.plot(line22[:,0],line22[:,1],linestyle=':',color='red')
 #ax2.plot(line11[:,0],line11[:,2]*np.square(0.8),linestyle=':',linewidth=0.5,color='blue',label='EPD ratio = 0.8')
 ax1.fill_between(line11[:,0],line11[:,1],line21[:,1],facecolor='green',alpha=0.5, label='$R_{pupil}/R_{lens}$ = 1.0')
 ax1.fill_between(line12[:,0],line12[:,1],line22[:,1],facecolor='red', alpha=0.5, label='$R_{pupil}/R_{lens}$ = 0.8')
-#ax1.set_ylim([0,0.25])
-ax2.set_xlabel('Total number of lenses')
+ax1.set_ylim([0,0.25])
+ax2.set_xlabel('Total number of lens assemblies')
 ax1.grid(True)
 ax1Ticks = ax1.get_xticks(minor=True)   
 ax2Ticks = ax1Ticks
@@ -61,8 +61,7 @@ ax2.set_xticks(ax2Ticks)
 ax2.set_xbound(ax1.get_xbound())
 ax2.set_xticklabels(tick_function(ax2Ticks),minor=True)
 #ax2.set_ylabel('geometrical efficiency',color='blue')
-ax1.set_ylabel('collection efficiency',color='black')
-ax1.set_xlabel('pixel per lens')
-ax1.legend()
-#ax2.legend(loc='upper right')
+ax1.set_ylabel('Collection efficiency',color='black')
+ax1.set_xlabel('Pixels per lens assembly')
+ax1.legend(loc='upper right')
 plt.show()
--- g4_plot.py
+++ g4_plot.py
@@ -1,8 +1,14 @@
 import matplotlib.patches as patches
+import argparse, os, pickle, glob
 import matplotlib.pyplot as plt
-import argparse, os, pickle
+from matplotlib import rcParams
 import iter_analysis as ia
 import numpy as np
+
+def set_style():
+        rcParams['lines.markersize'] = 4
+        rcParams['font.size'] = 20.0
+        rcParams['figure.figsize'] = (12, 9)
 
 
 def tick_function(X):
@@ -73,10 +79,29 @@ def plot_cl(path):
         plt.show()
         plt.close()
 
+def est_vals(method,c_sgn,c_bkg,s_eff):
+	if method == 'bootstrap':
+		bootstrp_arr = []
+		for i in xrange(20):
+			idx_s = np.random.choice(c_sgn.shape[0],c_sgn.shape[0])
+			idx_b = np.random.choice(c_bkg.shape[0],c_bkg.shape[0])
+			boot_s = c_sgn[idx_s]
+			boot_b = c_bkg[idx_b]
+			ks_bin = binning(boot_s,boot_b)
+			bootstrp_arr.append(ia.find_cl(1-np.cumsum(norm_hist(ks_bin,boot_b)),np.cumsum(norm_hist(ks_bin,boot_s)),1-s_eff))
+		return np.mean(bootstrp_arr),np.std(bootstrp_arr)
+	elif method == 'delta':
+		ks_bin = binning(c_sgn,c_bkg)
+		cdf_s = np.cumsum(norm_hist(ks_bin,c_sgn))
+		cdf_b = 1-np.cumsum(norm_hist(ks_bin,c_bkg))
+		idx = np.abs(cdf_s-s_eff).argmin()
+		der_s = np.mean(np.gradient(cdf_s,ks_bin[1])[idx-5:idx+5])
+		der_b = np.mean(np.gradient(cdf_b,ks_bin[1])[idx-5:idx+5])
+		return cdf_b[idx], np.sqrt(1.0/der_s*(s_eff)*(1-s_eff)/float(c_sgn.shape[0])*np.square(der_b)+(cdf_b[idx])*(1-cdf_b[idx])/float(c_bkg.shape[0]))
+
 
 def compare_bkgrej(seed_loc):
 	line11,line12,line21,line22 = [],[],[],[]
-	spl = 2
 	EPDR_flag = False
 	ext_rad_flag = False
 	ax1 = plt.subplot()
@@ -84,54 +109,59 @@ def compare_bkgrej(seed_loc):
 	ax1.set_xscale('log')
 	ax2.set_xscale('log')
 	for sl in seed_loc:
-		for root, _, files in os.walk('/farmshare/user_data/jdalmass/'):
-		    for fl in files:
-	       		if fl.endswith(sl+'electron-gammac2') and read_conf(root)['lens_system_name'] == 'Sam1' and root[37] == 'K' and read_conf(root)['EPD_ratio'] != 0.5:
-        	     		data = np.loadtxt(os.path.join(root, fl))
-			        c_sgn = data[0]
-			        c_bkg = data[1]
-				n = read_conf(root)['base']
-				max_diam = read_conf(root)['edge_length']/(n+np.sqrt(3)-1)
-				px_per_lens = sum(curved_surface2(read_conf(root)['detector_r'],max_diam,read_conf(root)['nsteps'],read_conf(root)['b_pixel']))
-			        ks_bin = binning(c_sgn,c_bkg)
-				conf_lev = [ia.find_cl(1-np.cumsum(norm_hist(ks_bin,c_b)),np.cumsum(norm_hist(ks_bin,c_s)),0.2) for c_s,c_b in zip(np.split(c_sgn,spl),np.split(c_bkg,spl))]
-				av_conf_lev = np.mean(conf_lev)
-				ax1.errorbar(px_per_lens, av_conf_lev, yerr=np.std(conf_lev)/np.sqrt(spl),fmt='o',markersize=3)
+		for root in glob.glob('/farmshare/user_data/jdalmass/*'):#/%selectron-gammac2'%sl):
+			fname = '%s/%selectron-gammac2'%(root,sl)
+			try:
+	       			if read_conf(root)['lens_system_name'] == 'Sam1' and root[37] == 'K' and read_conf(root)['EPD_ratio'] != 0.5:
+        	     			data = np.loadtxt(fname)
+			        	c_sgn = data[0]
+			        	c_bkg = data[1]
+					n = read_conf(root)['base']
+					max_diam = read_conf(root)['edge_length']/(n+np.sqrt(3)-1)
+					px_per_lens = sum(curved_surface2(read_conf(root)['detector_r'],max_diam,read_conf(root)['nsteps'],read_conf(root)['b_pixel']))
+					av_conf_lev,ste_conf_lev = est_vals('delta',c_sgn,c_bkg,0.8)
+					#ax1.errorbar(px_per_lens, av_conf_lev, yerr=ste_conf_lev,fmt='o',markersize=3)
 
-				if read_conf(root)['EPD_ratio'] == 1.0 and sl[:4] == 'r0-1':
-					line11.append([px_per_lens, av_conf_lev])
-              		        if read_conf(root)['EPD_ratio'] == 0.8 and sl[:4] == 'r0-1':
-					EPDR_flag = True
-                                	line12.append([px_per_lens, av_conf_lev])
-                      		if read_conf(root)['EPD_ratio'] == 1.0 and sl[:4] == 'r3-4':
-					ext_rad_flag = True
-                        	        line21.append([px_per_lens, av_conf_lev])
-                        	if read_conf(root)['EPD_ratio'] == 0.8 and sl[:4] == 'r3-4':
-                        	        line22.append([px_per_lens, av_conf_lev])
+					if read_conf(root)['EPD_ratio'] == 1.0 and sl[:4] == 'r0-1':
+						line11.append([px_per_lens, av_conf_lev, ste_conf_lev])
+              		        	if read_conf(root)['EPD_ratio'] == 0.8 and sl[:4] == 'r0-1':
+						EPDR_flag = True
+                                		line12.append([px_per_lens, av_conf_lev, ste_conf_lev])
+                      			if read_conf(root)['EPD_ratio'] == 1.0 and sl[:4] == 'r3-4':
+						ext_rad_flag = True
+                        		        line21.append([px_per_lens, av_conf_lev, ste_conf_lev])
+                        		if read_conf(root)['EPD_ratio'] == 0.8 and sl[:4] == 'r3-4':
+                        		        line22.append([px_per_lens, av_conf_lev, ste_conf_lev])
+			except IOError:
+				continue
 
 	line11 = np.asarray(sorted(line11,key=lambda x: x[0]))
 	line12 = np.asarray(sorted(line12,key=lambda x: x[0]))
 	line21 = np.asarray(sorted(line21,key=lambda x: x[0]))
 	line22 = np.asarray(sorted(line22,key=lambda x: x[0]))
-	ax1.plot(line11[:,0],line11[:,1],linestyle='-',color='green')
+	#ax1.plot(line11[:,0],line11[:,1],linestyle='-',color='green')
+	ax1.errorbar(line11[:,0],line11[:,1],line11[:,2],color='green',ms=8,fmt='o',linestyle='-')
 	if EPDR_flag:
-		ax1.plot(line12[:,0],line12[:,1],linestyle='-',color='red')
+		#ax1.plot(line12[:,0],line12[:,1],linestyle='-',color='red')
+		ax1.errorbar(line12[:,0],line12[:,1],line12[:,2],color='red',ms=8,fmt='o',linestyle='-')
 		if ext_rad_flag:
-			ax1.plot(line22[:,0],line22[:,1],linestyle=':',color='red')
-			ax1.fill_between(line12[:,0],line12[:,1],line22[:,1],facecolor='red', alpha=0.5, label='$R_{pupil}/R_{lens}$ = 0.8')
+			ax1.errorbar(line22[:,0],line22[:,1],line22[:,2],color='red',ms=8,fmt='o')
+			#ax1.plot(line22[:,0],line22[:,1],linestyle=':',color='red')
+			ax1.fill_between(line12[:,0],line12[:,1],line22[:,1],facecolor='none',linestyle='--',edgecolor='red',hatch='///',label='$R_{pupil}/R_{lens}$ = 0.8')
 	if ext_rad_flag:
-		ax1.plot(line21[:,0],line21[:,1],linestyle=':',color='green')
-		ax1.fill_between(line11[:,0],line11[:,1],line21[:,1],facecolor='green',alpha=0.5, label='$R_{pupil}/R_{lens}$ = 1.0')
+		ax1.errorbar(line21[:,0],line21[:,1],line21[:,2],color='green',ms=8,fmt='o')
+		#ax1.plot(line21[:,0],line21[:,1],linestyle=':',color='green')
+		ax1.fill_between(line11[:,0],line11[:,1],line21[:,1],facecolor='none',linestyle='--',edgecolor='green',hatch='\\\\\\',label='$R_{pupil}/R_{lens}$ = 1.0')
 	ax1.grid()
-	ax2.set_xlabel('Total number of lenses')
+	ax2.set_xlabel('Total number of lens assemblies')
 	ax1Ticks = ax1.get_xticks(minor=True)
 	ax2Ticks = ax1Ticks
 	ax2.set_xticks(ax2Ticks)
 	ax2.set_xbound(ax1.get_xbound())
 	ax2.set_xticklabels(tick_function(ax2Ticks),minor=True)
-	#ax1.set_title('comparison of different pupil aperture for a detector with %i px/lens'%px_per_lens)#('comparison of different configuration of 10k pixel detector')
-	ax1.set_xlabel('pixel per lens')#('$R_{pupil}/R_{lens}$')('pixel per lens')
-	ax1.set_ylabel('bkg rejection at 0.8 signal efficiency')
+	ax1.set_xlabel('Pixels per lens assembly')#('$R_{pupil}/R_{lens}$')('pixel per lens')
+	ax1.set_ylabel('Bkg rejection at 0.8 signal efficiency')
+	ax1.set_ylim([0.1,0.57])
 	ax1.legend()
 	plt.show()
 
@@ -141,6 +171,7 @@ if __name__ == '__main__':
 	parser.add_argument('path', help='select path-to-file or type compare')
         args = parser.parse_args()
         path = args.path
+	set_style()
 	if path == 'compare':
 		compare_bkgrej(['r0-1_2','r3-4_2'])
 	else:
